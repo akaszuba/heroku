84e908bd7a504379816a1eb229f6ae4ca5009e0d
akaszuba <akaszuba.poczta@gmail.com>
2020-03-24 16:03:25 +0100

    added bootstrap and jquery

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 module.exports = {
     index: function (req, res) {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('hello');
+        res.render('home.ejs', {
+            
+        });
     }
 }
--- server.js
+++ server.js
@@ -7,20 +7,21 @@ const app = express();
 const port = process.env.PORT;
 
 const db = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-db.connect((err) => {
-  if (err) {
-      throw err;
-  }
-  console.log('Connected to database');
-});
-global.db = db;
+// db.connect((err) => {
+//   if (err) {
+//       throw err;
+//   }
+//   console.log('Connected to database');
+// });
+// global.db = db;
 
 app.set('port',port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+app.use(express.static( __dirname + '/public'));
 
 app.get('/', home.index);
+
 
 app.listen(port,()=>{
   console.log(`Server running on port: ${port}`);
